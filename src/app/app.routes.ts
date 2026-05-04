28d71df27ab7cf4687f5c5a460ee5a1a12a7da01
ArmV17 <22040111@alumno.utc.edu.mx>
2026-05-04 13:57:54 -0600

    Structura Inicial de la Aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'nuevo-prestamo',
+    loadComponent: () => import('./pages/nuevo-prestamo/nuevo-prestamo.page').then( m => m.NuevoPrestamoPage)
+  },
+  {
+    path: 'devoluciones',
+    loadComponent: () => import('./pages/devoluciones/devoluciones.page').then( m => m.DevolucionesPage)
+  },
+  {
+    path: 'inventario',
+    loadComponent: () => import('./pages/inventario/inventario.page').then( m => m.InventarioPage)
+  },
 ];
