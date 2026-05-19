9959e8640a855442dabe4852706a405eb3a066ce
ArmV17 <22040111@alumno.utc.edu.mx>
2026-05-19 10:29:54 -0600

    Agregando logs

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-// 1. IMPORTA ESTA FUNCIÓN
 import { provideHttpClient } from '@angular/common/http'; 
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
-// IMPORTACIONES DE FIREBASE
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// 👇 1. CHIVATOS PARA VERIFICAR LAS LLAVES DE VERCEL
+console.log("🔥 Iniciando Angular en Vercel...");
+console.log("📦 Project ID inyectado:", environment.firebase.projectId);
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    
-    // 2. AGREGA ESTA LÍNEA AQUÍ
     provideHttpClient(), 
 
-    // CONFIGURACIÓN DE FIREBASE
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
   ],
-});
+// 👇 2. ATRAPAMOS CUALQUIER ERROR OCULTO Y LO OBLIGAMOS A MOSTRARSE EN ROJO
+}).catch(err => console.error('🚨 ERROR FATAL AL ARRANCAR ANGULAR:', err));
